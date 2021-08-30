726f768f60fa1102ebd217d1b7f251e8cf9a8b79
nuxion <nuxion@gmail.com>
2021-08-30 20:42:30 -0300

    Formulario para agregar la url

---
+++ urlalias/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import URLAlias
+
+class URLForm(forms.ModelForm):
+    # created_at = forms.DateField(label="Fecha de inicio", 
+    #    widget=forms.DateInput(attrs={"type": "date"}, format='%d/%m/%Y'),
+    #    input_formats=('%d/%m/%Y', ))
+    fullurl = forms.CharField(label="Ingrese la url a convertir")
+    alias = forms.CharField(label="alias")
+    class Meta:
+        model = URLAlias
+        fields = ['alias', 'fullurl']
--- urlalias/views.py
+++ urlalias/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import URLForm
 
 # Create your views here.
 
 def index(request):
 
-    ctx  = {"title": "Bienvenido al url alias"}
+
+    if request.method == "POST":
+        # Se puede acceder a la informacion del POST de forma
+        # manual
+        # request.POST["nombre"])
+        form = URLForm(request.POST)
+        if form.is_valid():
+            return HttpResponse(f"El formulario es valido") 
+        else:
+            return HttpResponse("El formulario es invalido") 
+    
+    form = URLForm()
+
+    ctx  = {"title": "Bienvenido al url alias", "form": form}
+	
 
     return render(request, "urlalias/home.html", ctx)
