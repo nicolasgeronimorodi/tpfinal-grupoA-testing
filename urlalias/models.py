e6d8eaabcb9f0a656dd042ef391c08e04ae20fec
nuxion <nuxion@gmail.com>
2021-08-30 20:03:02 -0300

    Modelo para URLAlias agregado

--- urlalias/models.py
+++ urlalias/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
+
+class URLAlias(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    alias = models.TextField(unique=True, null=False)
+    fullurl = models.TextField(null=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
